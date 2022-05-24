22cde75b514eeb8b7433bcaf03e435b810922d86
RaymundOfina <Raymund.ofina@gmail.com>
2022-05-23 20:36:48 -0700

    pushing up almost final code

---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO department(d_name) VALUES
+('CFO'),
+('Information Technology'),
+('Accounting'),
+('Human Resources');
+
+INSERT INTO roles(title, salary, department_id) VALUES
+('software Developer', 120000, 2),
+('Office Admin', 60000, 1),
+('UI/UX deisgner', 110000, 2),
+('IT Manager', 160000, 2),
+('Office Manager', 120000, 3),
+('Receptionist', 35000, 1),
+('Manager', 150000, 1),
+('Accountant', 180000, 3),
+('Delivery driver', 25000, 3;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Kevin', 'Smith', 1, NULL),
+('Tim', 'Na', 2, NULL),
+('Amanda', ' brown', 3, 2),
+('Madison', 'child', 4, 3);
+('Malia', 'Brown', 5, 4),
+('Sarah', 'Lourd', 6, NULL),
+('Tom', 'poe', 7, NULL);
